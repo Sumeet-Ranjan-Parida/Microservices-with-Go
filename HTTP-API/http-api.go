40c9bf54836dbcf57df3350d157d003353251d74
Sumeet Ranjan Parida <psumeet1998@gmail.com>
2020-11-06 17:09:46 +0530

    commit

---
+++ HTTP-API/http-api.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Book struct {
+	ID     string  `json:"id"`
+	Isbn   string  `json:"isbn"`
+	Title  string  `json:"title"`
+	Author *Author `json:"author"`
+}
+
+type Author struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+var books []Book
+
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
+}
+
+func getBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func createBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func updateBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func main() {
+	r := mux.NewRouter()
+
+	//Mock Data
+	books = append(books, Book{ID: "1", Isbn: "1111", Title: "Book One", Author: &Author{Firstname: "John", Lastname: "Doe"}})
+	books = append(books, Book{ID: "2", Isbn: "1122", Title: "Book Two", Author: &Author{Firstname: "Thomas", Lastname: "Steve"}})
+	books = append(books, Book{ID: "3", Isbn: "1133", Title: "Book Three", Author: &Author{Firstname: "Jack", Lastname: "Andrew"}})
+	books = append(books, Book{ID: "4", Isbn: "1144", Title: "Book Four", Author: &Author{Firstname: "Tim", Lastname: "C"}})
+
+	r.HandleFunc("/api/books", getBooks).Methods("GET")
+	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
+	r.HandleFunc("/api/books", createBook).Methods("POST")
+	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
+	r.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
